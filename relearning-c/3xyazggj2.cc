abb4a2fa7f7237911cc247472d4c732ecb7397b9
jay-io <jay.dnb@outlook.in>
2022-04-06 00:55:27 +0530

    Add file `3xyazggj2.cc`

---
+++ relearning-c/3xyazggj2.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int m;
+    printf("Enter month number: \n");
+    scanf("%d", &m);
+    
+    if (m == 1) printf("January");
+    if (m == 2) printf("February");
+    if (m == 3) printf("March");
+    if (m == 4) printf("April");
+    if (m == 5) printf("May");
+    if (m == 6) printf("June");
+    if (m == 7) printf("July");
+    if (m == 8) printf("August");
+    if (m == 9) printf("September");
+    if (m == 10) printf("October");
+    if (m == 11) printf("November");
+    if (m == 12) printf("December");
+}
