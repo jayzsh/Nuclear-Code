9995c95882465dd4031464c9a4cf2943d6b8f4cb
jay-io <jay.dnb@outlook.in>
2022-04-06 00:55:27 +0530

    Add file `3xxmvunps.cc`

---
+++ relearning-c/3xxmvunps.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf("I");
+    printf("\n");
+    printf("IN");
+    printf("\n");
+    printf("IND");
+    printf("\n");
+    printf("INDI");
+    printf("\n");
+    printf("INDIA");
+    printf("\n");
+    printf("INDIAN");
+}
